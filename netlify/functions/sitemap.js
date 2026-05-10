bc62a220b9b2a9f0b536b0bee7f064016d6df276
sreekanthm138 <sreekanthm138@gmail.com>
2026-05-10 21:13:31 +0530

    Sitemap Updated for SEO

--- netlify/functions/sitemap.js
+++ netlify/functions/sitemap.js
@@ -1,9 +1,9 @@
-// import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-// const supabase = createClient(
-//   process.env.VITE_SUPABASE_URL,
-//   process.env.VITE_SUPABASE_ANON_KEY
-// );
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
 export default async () => {
   try {
